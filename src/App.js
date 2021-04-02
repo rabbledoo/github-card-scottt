5a2b89d2e849f9523b20ba335475ac6e42799524
Scott Tarnowsky <scott.tarnowsky@gmail.com>
2021-04-02 09:45:47 -0400

    first attempt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import logo from "./logo.svg";
+import { Button, Card } from "react-bootstrap";
+import { useState } from "react";
 import "./App.css";
 
-function App() {
+const baseURL = "https://api.github.com/users/defunkt";
 
+function App() {
+  const [user, setUser] = useState({});
+  const [active, setActive] = useState(false);
+
+  // useEffect(() => {
+  //   let cardData = getUserInfo();
+  //   setUser(() => cardData);
+  // }, []);
+  // console.log(user);
+
+  let handleToggle = () => {
+    setActive(!active);
+    fetch(baseURL)
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  };
 
   return (
-   <button>Toggle</button>
+    <>
+      {active ? (
+        <Card>
+          <Card.Img src={user.avatar_url} />
+          <Card.Body>
+            <Card.Title>{user.name}</Card.Title>
+            <Card.Text>
+              <p>{user.bio}</p>
+              <p>Location: {user.location}</p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ) : (
+        " "
+      )}
+      <Button onClick={handleToggle}>Toggle</Button>;
+    </>
   );
 }
 
